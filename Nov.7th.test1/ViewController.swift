03a01195fd516bd28ae8256ee068efcf9ffbd5a5
libin0205 <1611774332@qq.com>
2019-01-05 08:50:51 +0800

    1.5

--- Nov.7th.test1/ViewController.swift
+++ Nov.7th.test1/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var value: UITextField!
+    @IBOutlet weak var value1: UITextField!
+    @IBOutlet weak var value2: UITextField!
+    
+    @IBAction func StartButton(_ sender: Any) {
+        cal(x: Int(value!)!,y: Int(value1!)!,z: Int(value2!)!                                                                                                                                                                       )->Int;
+    }
+    func cal(x:Int,y:Int,z:Int)->Int
+    {
+        var days:Int=0
+        var biryear:Int=1998
+        var birthmon:Int=2
+        var birthday:Int=5
+        biryear=x-biryear
+        birthmon=y-birthmon
+        birthday=z-birthday
+        if(biryear<0)
+        {
+            print("error")
+        }
+        if(birthmon<0){
+            biryear=biryear-1;
+        }
+        if(birthday<0)
+        {
+            birthmon=birthmon-1
+        }
+        days=biryear*365+birthmon*30+birthday
+        return days
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
